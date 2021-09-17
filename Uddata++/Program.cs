9352fd5dc81b42d39fbbf7a06f2411623729a6ee
Joey Knutsson <joey.knutsson@rocketmail.com>
2021-09-17 11:52:17 +0200

    Add project files.

---
+++ Uddata++/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uddata__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coffeclub = "";
+            Console.WriteLine("Hello World!");
+
+            StudentClass student= new StudentClass();
+            student.Name = "Joey";
+            student.Warnings = 0;
+            student.Subject = Subjects.Prog;
+            student.Grade = (Grade)10;
+
+            TeacherClass teacher = new TeacherClass();
+            teacher.Name = "Henrik";
+            teacher.CoffeeClub = true;
+            teacher.Subject = Subjects.Prog;
+
+            SubjectClass subject = new SubjectClass();
+            subject.Name = "Programming";
+            subject.Subject = Subjects.Prog;
+
+            //foreach (var Grades in Enum.GetValues(typeof(Grade)))
+            //{
+            //    Console.WriteLine((int)Grades + " " + Grades.ToString());
+            //}
+
+            if (teacher.CoffeeClub == true) { coffeclub = " har et medlemskab til kaffeklubben"; }
+            else { coffeclub = " har ikke et medlemskab til kaffeklubben"; }
+
+            Console.WriteLine($"{student.Name} har {student.Subject} hvor han har fået {(int)student.Grade} en {student.Grade} karakter. Han har {student.Warnings} advarseler.");
+            Console.WriteLine($"{teacher.Name} underviser i {teacher.Subject}. {teacher.Name}{coffeclub}");
+            Console.WriteLine($"{subject.Subject} er en forkotelse for {subject.Name}");
+        }
+    }
+}
---
+++ Uddata++/SubjectClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uddata__
+{
+    public enum Subjects { Dan, Eng, Math, His, Geo, Prog }
+    public enum Grade { ringe = -3, utilstrækkelige = 0, tilstrækkelig = 02, jævn = 4, god = 7, super = 10, fremragende = 12}
+    class SubjectClass : ParentClass
+    {
+        public Grade Grade { get; set; }
+        public Subjects Subject {  get; set; }
+    }
+}
